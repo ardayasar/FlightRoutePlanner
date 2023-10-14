68dd1f3088978c07a8ef1561dc58484df62a576f
ardayasar <erddoerdo12@gmail.com>
2023-10-14 18:54:39 +0300

    Generation of route made

--- routePlannerByRealData.py
+++ routePlannerByRealData.py
@@ -1,6 +1,8 @@
 import json
 import matplotlib.pyplot as plt
 import random
+import bezier
+import numpy as np
 
 
 def random_color():
@@ -24,7 +26,7 @@ def get_runway_point(airport_runway_code, point_type="start"):
     return None
 
 
-MILES_EXTENSION = 250
+MILES_EXTENSION = 350
 
 
 def calculate_extension(start_point, end_point, miles):
@@ -41,26 +43,60 @@ def calculate_extension(start_point, end_point, miles):
     return (extended_x, extended_z)
 
 
-selection_one = "KCIA-RW18"
-selection_two = "KKBI-RW29"
+def generateRoute(selection_one, selection_two):
+    point_one = get_runway_point(selection_one, "start")
+    point_two = get_runway_point(selection_two, "end")
 
-point_one = get_runway_point(selection_one, "start")
-point_two = get_runway_point(selection_two, "end")
+    if point_one and point_two:
+        runway_end_one = get_runway_point(selection_one, "end")
+        runway_start_two = get_runway_point(selection_two, "start")
 
-if point_one and point_two:
-    runway_end_one = get_runway_point(selection_one, "end")
-    extended_takeoff_point = calculate_extension(point_one, runway_end_one, MILES_EXTENSION)
+        extended_takeoff_point = calculate_extension(point_one, runway_end_one, MILES_EXTENSION)
+        extended_landing_point = calculate_extension(runway_start_two, point_two, MILES_EXTENSION)
 
-    runway_start_two = get_runway_point(selection_two, "start")
-    extended_landing_point = calculate_extension(runway_start_two, point_two, MILES_EXTENSION)
+        curve_start_takeoff_point = calculate_extension(point_one, runway_end_one, MILES_EXTENSION - 60)
+        curve_end_landing_point = calculate_extension(runway_start_two, point_two, MILES_EXTENSION - 60)
 
-    ax.plot([point_one[0], runway_end_one[0], extended_takeoff_point[0], extended_landing_point[0], point_two[0],
-             runway_start_two[0]],
-            [point_one[2], runway_end_one[2], extended_takeoff_point[1], extended_landing_point[1], point_two[2],
-             runway_start_two[2]], 'purple', linestyle='--')
+        curve_start_landing_point = calculate_extension([extended_takeoff_point[0], 0, extended_takeoff_point[1]],
+                                                        [extended_landing_point[0], 0, extended_landing_point[1]], -60)
+        curve_end_takeoff_point = calculate_extension([extended_landing_point[0], 0, extended_landing_point[1]],
+                                                      [extended_takeoff_point[0], 0, extended_takeoff_point[1]], -60)
 
-    ax.scatter(extended_takeoff_point[0], extended_takeoff_point[1], c='blue', marker='o')
-    ax.scatter(extended_landing_point[0], extended_landing_point[1], c='red', marker='o')
+        ax.plot([runway_end_one[0], curve_start_takeoff_point[0]],
+                [runway_end_one[2], curve_start_takeoff_point[1]], 'blue', linestyle='--')
+
+        nodes_takeoff = np.asfortranarray(
+            [
+                [curve_start_takeoff_point[0], extended_takeoff_point[0], curve_end_takeoff_point[0]],
+                [curve_start_takeoff_point[1], extended_takeoff_point[1], curve_end_takeoff_point[1]]
+            ]
+        )
+        curve_takeoff = bezier.Curve(nodes_takeoff, degree=2)
+        ax.plot(*curve_takeoff.evaluate_multi(np.linspace(0.0, 1.0, 256)), 'blue')
+
+        ax.plot([curve_end_takeoff_point[0], curve_start_landing_point[0]],
+                [curve_end_takeoff_point[1], curve_start_landing_point[1]], 'blue', linestyle='--')
+
+        nodes_landing = np.asfortranarray(
+            [
+                [curve_start_landing_point[0], extended_landing_point[0], curve_end_landing_point[0]],
+                [curve_start_landing_point[1], extended_landing_point[1], curve_end_landing_point[1]]
+            ]
+        )
+        curve_landing = bezier.Curve(nodes_landing, degree=2)
+        ax.plot(*curve_landing.evaluate_multi(np.linspace(0.0, 1.0, 256)), 'blue')
+
+        ax.plot([curve_end_landing_point[0], runway_start_two[0]],
+                [curve_end_landing_point[1], runway_start_two[2]], 'blue', linestyle='--')
+
+        # Scatter points
+        # ax.scatter(extended_takeoff_point[0], extended_takeoff_point[1], c='blue', marker='o')
+        # ax.scatter(extended_landing_point[0], extended_landing_point[1], c='red', marker='o')
+        # ax.scatter(curve_start_takeoff_point[0], curve_start_takeoff_point[1], c='red', marker='o')
+        # ax.scatter(curve_end_landing_point[0], curve_end_landing_point[1], c='red', marker='o')
+        # ax.scatter(curve_end_takeoff_point[0], curve_end_takeoff_point[1], c='green', marker='o')
+        # ax.scatter(curve_start_landing_point[0], curve_start_landing_point[1], c='green', marker='o')
+
 
 for airport_code, airport_data in data.items():
     current_color = random_color()
@@ -73,6 +109,13 @@ for airport_code, airport_data in data.items():
 
         ax.scatter([runway_start_x, runway_end_x], [runway_start_z, runway_end_z], c=current_color, marker='x')
         ax.plot([runway_start_x, runway_end_x], [runway_start_z, runway_end_z], color=current_color)
+
+
+route = [["KCIA-RW36", "KKBI-RW29"], ["KKBI-RW12", "KNET-RW24"]]
+
+for ar in route:
+    generateRoute(ar[0], ar[1])
+
 
 ax.invert_yaxis()
 ax.set_xlabel('X Coordinate')
